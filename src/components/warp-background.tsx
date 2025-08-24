fa7b6e81bff7af4466a6eadc252986dbb7b2264d
Riccardo Gualtieri <77571958+Rai-Rock@users.noreply.github.com>
2025-08-24 13:59:22 +0000

    first changes

--- src/components/warp-background.tsx
+++ src/components/warp-background.tsx
@@ -1,9 +1,9 @@
 import { Warp, type WarpProps } from '@paper-design/shaders-react';
 
 export default function WarpBackground(props: WarpProps) {
-    
+
     const defaultProps = {
-        speed: 0.4,
+        speed: 0.9,
         rotation: 0.5,
         style: { width: '100%', height: '100%' }
     };
